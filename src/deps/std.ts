576263d59597f83c842ec37dfe6f4ae4b640d7d7
uga-rosa <uga6603@gmail.com>
2024-01-14 12:10:33 +0900

    Test for main.ts (WIP)

--- src/deps/std.ts
+++ src/deps/std.ts
@@ -3,3 +3,4 @@ export * as TOML from "https://deno.land/std@0.212.0/toml/mod.ts";
 export * as YAML from "https://deno.land/std@0.212.0/yaml/mod.ts";
 export { assert, assertEquals, assertThrows } from "https://deno.land/std@0.212.0/assert/mod.ts";
 export * as bytes from "https://deno.land/std@0.212.0/bytes/mod.ts";
+export * as path from "https://deno.land/std@0.212.0/path/mod.ts";
